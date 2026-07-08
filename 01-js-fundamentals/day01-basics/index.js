1d951863f67f3ddb59aefeb9a028a64d2b1f59f7
Ashish Jha <ashishjha1304@gmail.com>
2026-07-08 18:21:42 +0530

    refactor: update problem statement formatting and improve function structure

--- 01-js-fundamentals/day01-basics/index.js
+++ 01-js-fundamentals/day01-basics/index.js
@@ -1,1 +1,29 @@
-// #1 Problem - Reverse a string without using .reverse()
+// Problem #1: Reverse a string without using .reverse()
+
+function reverse(str) {
+  let temp = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    temp += str[i];
+  }
+
+  return temp;
+}
+
+let name = "genny";
+// console.log(`Reversed string: ${reverse(name)}`);
+
+// Problem #2: Write a Function that returns largest number in an array, without Math.max
+
+function maxNum(arr) {
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+// let arr = [1, 56, 34, 20, 95];
+let arr = [-5, -10, -2];
+// console.log(`The largest number in the array is: ${maxNum(arr)}`);
